1c9dbfc66987d2e8137cd9d7004b422c90ea4e23
David Sanchez <fernandoge210895@gmail.com>
2022-12-27 16:12:59 -0500

    Table component

--- src/bemit/components/Table/TableCell/TableCell.tsx
+++ src/bemit/components/Table/TableCell/TableCell.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 import { AiOutlineArrowUp } from 'react-icons/ai';
 
+import './c-table-cell.scss';
+
+
+
 interface Props {
     children: React.ReactNode;
     sortDirection: 'asc' | 'desc';
+    align: 'left' | 'center' | 'right';
 }
 
 
 const TableCell: FC<Props> = ({
     children,
+    align = 'left',
     sortDirection = 'asc'
 }) => {
     return (
